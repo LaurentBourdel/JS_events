e294ee1e0a123c8fe75d56e287f03c7444d7f8ce
LaurentBourdel <62954361+LaurentBourdel@users.noreply.github.com>
2020-05-25 15:43:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+
+// exo 1 
+
+var myFooter = document.getElementsByTagName('footer')[0];
+
+function onFoorterClick() { 
+    console.log('clique');
+}
+
+myFooter.addEventListener("click", onFoorterClick);
+
+// exo 1-bis
+
+var myFooter = document.querySelector('footer');
+var x=1;
+function onFoorterClick() { 
+    console.log('clic numéro '+ x);
+    x++
+}
+
+myFooter.addEventListener("click", onFoorterClick);
+
+// exo 2
+
+var menuHamb = document.querySelector('button');
+var navbarHeader = document.getElementById('navbarHeader');
+menuHamb.addEventListener("click", toggleOn); 
+
+function toggleOn() {
+    navbarHeader.classList.toggle("collapse");
+};
+
+// exo 3
+
+var myEditButton = document.getElementsByClassName('btn btn-sm btn-outline-secondary')[0];
+
+myEditButton.addEventListener('click', putRedText);
+
+function putRedText() {
+    var cardChange = document.querySelector('p.card-text');
+    cardChange.style.color = 'red';
+}
+
+// exo 4
+
+var myEditButton2 = document.querySelectorAll('div.btn-group')[1].children[1]; 
+myEditButton2.addEventListener('click', putGreenText1);
+
+var statusButton = false;  
+
+function putGreenText1() {
+    var cardChange2 = document.querySelectorAll('p.card-text');
+    if (statusButton) {         
+        statusButton = false;
+        cardChange2[1].style.color = '';
+    } else {               
+        statusButton = true;
+        cardChange2[1].style.color = 'green';
+    }
+}
+
+// exo 5
+
+var cdnBootstrap = document.getElementsByTagName('link')[0];
+var doubleClicNavbar = document.querySelector('header');
+var headTag = document.querySelector('head');
+var statusHeader = false;
+
+var changeCDN = function () {
+  if (statusHeader == false) {
+    cdnBootstrap.parentNode.removeChild(cdnBootstrap);
+    statusHeader = true;
+  } else if (statusHeader == true) {
+    headTag.appendChild(cdnBootstrap);
+    statusHeader= false;
+  }
+};
+
+doubleClicNavbar.addEventListener('dblclick', changeCDN);
+
+//exo6
+
+var onClick = function(e) {
+  var stashePic = document.getElementById("mustache-pic");  
+    stashePic.style.top = e.clientX + "px";
+    stashePic.style.left = e.clientY + "px";
+  };
+
+addEventListener("click", onClick);
